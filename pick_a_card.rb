67bb1f88e1539c68399a8ec17e8c19e3a4cb1d39
bohdanshvetc <dan6147@ukr.net>
2022-02-11 12:39:49 +0200

    Добавил картинки мастей

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,11 +1,13 @@
 # Програмка на генерацию случайной карты
 values = %i[2 3 4 5 6 7 8 9 10 J Q K T]
-suits =  %i[Diamonds Hearts Clubs Spades]
+suits =  %i[♦ ♠ ♥ ♣]
 
 20.times do
-print "#{values.sample} of #{suits.sample}\r"
-sleep 0.1
+  
+  print "#{values.sample}#{suits.sample}"
+  sleep 0.1
+  system("clear")
 end
 
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
