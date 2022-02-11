e90f231448c5ef1ebb14f39ba53083c6b84b5fe1
bohdanshvetc <dan6147@ukr.net>
2022-02-11 11:53:10 +0200

    Добавил визуальную прокрутку

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,4 +2,10 @@
 values = %i[2 3 4 5 6 7 8 9 10 J Q K T]
 suits =  %i[Diamonds Hearts Clubs Spades]
 
+20.times do
+print "#{values.sample} of #{suits.sample}\r"
+sleep 0.1
+end
+
+
 puts "#{values.sample} of #{suits.sample}"
